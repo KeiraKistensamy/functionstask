34f1b3c0713616997c729bd69447795fc16ce7a7
KeiraKistensamy <Keirakist0987@gmail.com>
2024-05-23 09:14:16 +0200

    functionstask

---
+++ script/code.js
@@ -0,0 +1,22 @@
+// function greeting() {
+// console.log(`Hello!`)
+// }
+// greeting ()
+
+// **********************************************************************************
+
+// let multiplication = (numb1, numb2) => {
+//     console.log(numb1*numb2);
+// }
+// multiplication (5, 5)
+
+// *********************************************************************************
+
+// function tableOfFive (n){
+// let result = n * 5
+// console.log(result);
+// return result
+// }
+// tableOfFive(3)
+// tableOfFive(7)
+// tableOfFive(10)
